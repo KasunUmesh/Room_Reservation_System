e92ff3e84b6b8ee0d0e2b639fa0932c7c50b8816
KasunUmesh <kasunumesh38@gmail.com>
2022-09-20 02:42:11 +0530

    Creat Login Page

---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+    public JFXComboBox cmbPosition;
+
+    public void btnSignInOnAction(ActionEvent actionEvent) {
+    }
+}
