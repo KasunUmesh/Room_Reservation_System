44f505643f06f7df7db8800aab8a405b8671706e
KasunUmesh <kasunumesh38@gmail.com>
2022-09-21 01:28:18 +0530

    Create Admin DashBoard

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,15 +1,45 @@
 package controller;
 
 import com.jfoenix.controls.JFXComboBox;
-import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public TextField txtUserName;
     public PasswordField txtPassword;
-    public JFXComboBox cmbPosition;
+    public JFXComboBox<String> cmbPosition;
+    public AnchorPane root;
 
-    public void btnSignInOnAction(ActionEvent actionEvent) {
+    public void initialize(){
+
+        ObservableList<String> list = FXCollections.observableArrayList("Admin", "Receptionist");
+        cmbPosition.setItems(list);
+
+    }
+
+    public void btnSignInOnAction() throws IOException {
+
+        if (cmbPosition.getValue() == "Admin"){
+
+            Parent parent = FXMLLoader.load(getClass().getResource("../view/AdminDashBoardForm.fxml"));
+            Scene scene = new Scene(parent);
+
+            Stage primaryStage = (Stage) this.root.getScene().getWindow();
+
+            primaryStage.setScene(scene);
+            primaryStage.centerOnScreen();
+            primaryStage.setResizable(false);
+
+        }
+
     }
 }
